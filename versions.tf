52639e6661c432bc7da682cd8c901b5894fe6446
Shlomi-Lantser <92504985+Shlomi-Lantser@users.noreply.github.com>
2026-03-05 16:48:25 +0200

    Deleted unnecessary comments

--- versions.tf
+++ versions.tf
@@ -4,12 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 6.0",
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 3.0"
-    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.36"
@@ -17,47 +14,23 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "tf-state-test-t4"
+    bucket         = "terraform-tfstates-shlomi"
     key            = "eks-hello-world/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-1"
     encrypt        = true
     use_lockfile = true
+    profile = "assignment-profile"
   }
  }
 
 provider "aws" {
-  region = var.aws_region
-
+  region  = var.aws_region
+  
   default_tags {
     tags = {
       Project     = var.project_name
       Environment = var.environment
       ManagedBy   = "terraform"
-    }
-  }
-}
-
-# Kubernetes and Helm providers are configured dynamically using EKS cluster outputs
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.name]
-  }
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.name]
     }
   }
 }
